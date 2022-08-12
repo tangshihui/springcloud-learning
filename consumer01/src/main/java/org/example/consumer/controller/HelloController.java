66af3816b938577794d373ada6fd95cafe293eaa
tangshihui <tsh_index@163.com>
2022-08-12 14:25:56 +0800

    apply logback

--- consumer01/src/main/java/org/example/consumer/controller/HelloController.java
+++ consumer01/src/main/java/org/example/consumer/controller/HelloController.java
@@ -1,19 +1,27 @@
 package org.example.consumer.controller;
 
 import org.example.consumer.feign.service.FeignCalculatorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consume")
 public class HelloController {
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @Autowired
     private FeignCalculatorService calculatorService;
 
 
-    @GetMapping("/{name}")
+    @GetMapping("/hello/{name}")
     public String hello(@PathVariable("name") String name) {
+        logger.debug("debug message");
+        logger.info("info message");
+        logger.warn("warn message");
+        logger.error("error message");
+
         return calculatorService.hello(name);
     }
 
