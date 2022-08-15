2114e81787b8a722426c05d2afd9be26b01c9624
tangshihui <tsh_index@163.com>
2022-08-15 14:18:10 +0800

    add provider

---
+++ consumer01/src/main/java/org/example/consumer/feign/service/FeignHelloService.java
@@ -0,0 +1,15 @@
+package org.example.consumer.feign.service;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "provider01", path = "/hello")
+public interface FeignHelloService {
+
+    @GetMapping("/{name}")
+    String hello(@PathVariable("name") String name);
+
+}
