ae434e6e1d3b488696d30a159359740d30283250
tangshihui <tsh_index@163.com>
2022-09-19 15:37:08 +0800

    add disable exception handler

--- exception-spring-boot-starter/src/main/java/org/example/exception/config/GlobalExceptionHandlerConfig.java
+++ exception-spring-boot-starter/src/main/java/org/example/exception/config/GlobalExceptionHandlerConfig.java
@@ -2,6 +2,8 @@ package org.example.exception.config;
 
 
 import org.example.exception.controller.GlobalExceptionHandler;
+import org.example.exception.interceptor.GlobalInterceptor;
+import org.example.exception.interceptor.WebConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +13,15 @@ public class GlobalExceptionHandlerConfig {
     @Bean
     public GlobalExceptionHandler newGlobalExceptionHandler(){
         return new GlobalExceptionHandler();
+    }
+
+    @Bean
+    public GlobalInterceptor newGlobalInterceptor(){
+        return new GlobalInterceptor();
+    }
+
+    @Bean
+    public WebConfig newWebconfig(){
+        return new WebConfig();
     }
 }
--- exception-spring-boot-starter/src/main/java/org/example/exception/interceptor/GlobalInterceptor.java
+++ exception-spring-boot-starter/src/main/java/org/example/exception/interceptor/GlobalInterceptor.java
@@ -34,9 +34,7 @@ public class GlobalInterceptor implements HandlerInterceptor {
             final HandlerMethod handlerMethod = (HandlerMethod) handler;
             final Class<?> clazz = handlerMethod.getBeanType();
             final Method method = handlerMethod.getMethod();
-            // 判断是否在类对象上添加了注解
             if (clazz.isAnnotationPresent(DisableGlobalExceptionHandler.class)) {
-                // 设置此请求返回体，需要包装，往下传递，在ResponseBodyAdvice接口进行判断
                 request.setAttribute("DisableGlobalExceptionHandler", clazz.getAnnotation(DisableGlobalExceptionHandler.class));
             } else if (method.isAnnotationPresent(DisableGlobalExceptionHandler.class)) {
                 request.setAttribute("DisableGlobalExceptionHandler", method.getAnnotation(DisableGlobalExceptionHandler.class));
